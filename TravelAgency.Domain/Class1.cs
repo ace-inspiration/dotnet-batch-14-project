483d14f7e6d9ec09c040a3e1abf3ec19e138b368
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-30 22:53:09 +0630

    feat: add example feature

--- TravelAgency.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TravelAgency.Domain
-{
-	public class Class1
-	{
-
-	}
-}
---
+++ TravelAgency.Domain/Features/BookingByUserId/BookingByUserIdController.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelAgency.Domain.Features.BookingByUserId;
+
+internal class BookingByUserIdController : ControllerBase
+{
+}
