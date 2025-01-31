c31677d86c58a528f1be4f582d67903179dc3a78
Hnin Wai Hlaing <hninwaihlaing.dev@gmail.com>
2025-01-31 21:51:59 +0630

    feat: Add UserLists, PaymentListByUserId

---
+++ TravelAgency.Domain/Features/PaymentListByUserId/PaymentListByUserIdController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Domain.Features.BookingListByUserId;
+
+namespace TravelAgency.Domain.Features.PaymentListByUserId
+{
+    [Route("api/users")]
+    [ApiController]
+    public class PaymentListByUserIdController : ControllerBase
+    {
+        private readonly PaymentListByUserIdService _service;
+        public PaymentListByUserIdController(PaymentListByUserIdService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}/payments")]
+        public async Task<IActionResult> Execute(string id)
+        {
+            var response = await _service.Execute(id);
+
+            return response.Success ? Ok(response) : StatusCode(500, response);
+        }
+    }
+}
---
+++ TravelAgency.Domain/Features/UserLists/UserListController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Domain.Features.BookingListByUserId;
+
+namespace TravelAgency.Domain.Features.UserLists
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserListController : ControllerBase
+    {
+        private readonly UserListService _service;
+        public UserListController(UserListService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Userlist()
+        {
+            var response = await _service.GetUserList();
+
+            return response.Success ? Ok(response) : StatusCode(500, response);
+        }
+    }
+}
