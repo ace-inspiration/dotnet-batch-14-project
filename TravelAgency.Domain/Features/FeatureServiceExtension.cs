e198e74f3e9ecfdd5fd96a8c8db838fc34268437
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2025-01-31 19:35:34 +0630

    feat: inject TravelPackageService

--- TravelAgency.Domain/Features/FeatureServiceExtension.cs
+++ TravelAgency.Domain/Features/FeatureServiceExtension.cs
@@ -5,6 +5,7 @@ using TravelAgency.Domain.Features.BookingFeatures;
 using TravelAgency.Domain.Features.BookingListByUserId;
 using TravelAgency.Domain.Features.Login;
 using TravelAgency.Domain.Features.PaymentFeature;
+using TravelAgency.Domain.Features.TravelPackage;
 using TravelAgency.Domain.Features.UserRegister;
 
 namespace TravelAgency.Domain.Features;
@@ -22,5 +23,7 @@ public static class FeatureServiceExtension
 		builder.Services.AddScoped<PaymentService>();
 
 		builder.Services.AddScoped<LoginService>();
+
+		builder.Services.AddScoped<TravelPackageService>();
 	}
 }
