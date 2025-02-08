ca3be1ed8edc3a225a200f405f6736b9fb977ef6
May Thazin Oo <maythazinoo18817@gmail.com>
2025-02-08 12:24:24 +0630

    fix: add models

---
+++ TravelAgency.Database/AppDbContextModels/AppDbContext.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TravelAgency.Database.AppDbContextModels;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Booking> Bookings { get; set; }
+
+    public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<TravelPackage> TravelPackages { get; set; }
+
+    public virtual DbSet<Traveler> Travelers { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=TravelAgencyDb;User Id=sa;Password = mtzoo@123;TrustServercertificate= true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Bookings__3213E83F4782F263");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasDefaultValueSql("(newid())")
+                .HasColumnName("id");
+            entity.Property(e => e.BookingDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("booking_date");
+            entity.Property(e => e.NumberOfTravelers).HasColumnName("number_of_travelers");
+            entity.Property(e => e.Status)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('pending')")
+                .HasColumnName("status");
+            entity.Property(e => e.TotalPrice)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("total_price");
+            entity.Property(e => e.TravelPackageId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("travel_package_id");
+            entity.Property(e => e.UserId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("user_id");
+        });
+
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Payments__3213E83FC912D91D");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("id");
+            entity.Property(e => e.Amount)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("amount");
+            entity.Property(e => e.BookingId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("booking_id");
+            entity.Property(e => e.PaymentDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("payment_date");
+            entity.Property(e => e.PaymentStatus)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('pending')")
+                .HasColumnName("payment_status");
+            entity.Property(e => e.UserId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("user_id");
+        });
+
+        modelBuilder.Entity<TravelPackage>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__TravelPa__3213E83F375A2FB2");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("id");
+            entity.Property(e => e.CancellationPolicy)
+                .HasColumnType("text")
+                .HasColumnName("cancellation_policy");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.Destination)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("destination");
+            entity.Property(e => e.Duration).HasColumnName("duration");
+            entity.Property(e => e.Inclusions)
+                .HasColumnType("text")
+                .HasColumnName("inclusions");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("price");
+            entity.Property(e => e.Status)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('activate')")
+                .HasColumnName("status");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("title");
+        });
+
+        modelBuilder.Entity<Traveler>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Traveler__3213E83FEAC31D9B");
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.BookingId)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("booking_id");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('male')")
+                .HasColumnName("gender");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3213E83FB677C58E");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__AB6E61640802EF5A").IsUnique();
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(36)
+                .IsUnicode(false)
+                .HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.PasswordHash)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("password_hash");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("phone");
+            entity.Property(e => e.Role)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasDefaultValueSql("('customer')")
+                .HasColumnName("role");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TravelAgency.Database/AppDbContextModels/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelAgency.Database.AppDbContextModels;
+
+public partial class Booking
+{
+    public string Id { get; set; } = null!;
+
+    public string UserId { get; set; } = null!;
+
+    public string TravelPackageId { get; set; } = null!;
+
+    public int NumberOfTravelers { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public DateTime BookingDate { get; set; }
+
+    public string Status { get; set; } = null!;
+}
