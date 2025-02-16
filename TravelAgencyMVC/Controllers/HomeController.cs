eb072bdb081cc2c8ff2be7f3142c69fc7d72a2f8
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-02-16 11:04:35 +0630

    Feat: packages

--- TravelAgencyMVC/Controllers/HomeController.cs
+++ TravelAgencyMVC/Controllers/HomeController.cs
@@ -1,45 +1,52 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TravelAgency.Domain.Features.TravelPackages;
 using TravelAgencyMVC.Models;
 
-namespace TravelAgencyMVC.Controllers
+namespace TravelAgencyMVC.Controllers;
+
+[Authorize]
+public class HomeController : Controller
 {
-    [Authorize]
-    public class HomeController : Controller
+    
+    private readonly TravelPackageService _travelPackageService;
+
+   public HomeController (TravelPackageService travelPackageService)
     {
-        private readonly ILogger<HomeController> _logger;
+        _travelPackageService = travelPackageService;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
+    public IActionResult Index()
+    {
+        if (User.IsInRole("admin"))
         {
-            _logger = logger;
+            return RedirectToAction("AdminDashboard", "Admin");
         }
 
-        public IActionResult Index()
-        {
-            if (User.IsInRole("admin"))
-            {
-                return RedirectToAction("AdminDashboard", "Admin");
-            }
+        ViewBag.UserName = User.Identity.Name;
+        return View();
+    }
 
-            ViewBag.UserName = User.Identity.Name;
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
 
-        public IActionResult AccessDenied()
-        {
-            return View();
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public IActionResult Packages ()
+    {
+        var lst = _travelPackageService.Execute();
+        return View("Packages", lst);
     }
 }
--- TravelAgencyMVC/Controllers/UserController.cs
+++ TravelAgencyMVC/Controllers/UserController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TravelAgency.Domain.Features.TravelPackages;
 
 namespace TravelAgencyMVC.Controllers
 {
+
+    
     public class UserController : Controller
     {
+
+       private readonly TravelPackageService _travelPackageService;
+
+        public UserController (TravelPackageService travelPackageService)
+        {
+            _travelPackageService = travelPackageService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var Lst = _travelPackageService.Execute();
+            return View("Packages");
         }
     }
 }
